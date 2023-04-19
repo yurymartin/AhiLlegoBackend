9cc00d7f4d8cf44415378a6cdcc15fffd8b7d590
Yury Martin <martinyuri299@gmail.com>
2023-04-19 15:32:30 -0500

    agregar __dirname para firebase

--- src/firebase/services/connection/connection.service.ts
+++ src/firebase/services/connection/connection.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
+import * as path from 'path';
 
 @Injectable()
 export class ConnectionService {
@@ -11,11 +12,11 @@ export class ConnectionService {
       const serviceAccount = this.configService.get<string>(
         'FIREBASE_CREDENTIALS',
       );
+      const absolutePath = path.join(__dirname, '../../../../', serviceAccount);
 
       const app = admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert(absolutePath),
       });
-
       return app;
     } catch (ex) {
       console.log('ERROR initializeFirebase', ex);
